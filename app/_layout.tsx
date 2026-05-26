102ed4bea16965d725c397e6815aa39dade94954
momo-glist <110183435+momo-glist@users.noreply.github.com>
2026-05-26 21:44:30 +0000

    ...

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  const [fontLoaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+  return <Stack />;
 }
---
+++ supabase/migrations/20260526212005_profile_migration.sql
@@ -0,0 +1,54 @@
+create table if not exists profiles(
+    id uuid primary key references auth.users not null,
+    full_name text,
+    english_level text,
+    motivations text[],
+    interests text[],
+    onboarding_completed boolean default false,
+    is_premium boolean default false,
+    premium_expires_at timestamp with time zone, 
+    updated_at timestamp with time zone default now()
+);
+
+alter table public.profiles enable row level security;
+
+create policy "User can read own profile"
+on public.profiles
+for select 
+using (auth.uid() =  id);
+
+create policy "User can insert own profile"
+on public.profiles
+for insert 
+with check (auth.uid() =  id);
+
+create policy "User can update own profile"
+on public.profiles
+for update 
+using (auth.uid() =  id);
+with check (auth.uid() =  id);
+
+revoke update on table public.profiles from authenticated;
+revoke insert on table public.profiles from authenticated;
+
+grant select on table public.profiles to authenticated;
+
+grant insert (
+    id,
+    full_name,
+    english_level,
+    motivations,
+    interests,
+    onboarding_completed,
+    updated_at
+) on table public.profiles to authenticated;
+
+grant update (
+    id,
+    full_name,
+    english_level,
+    motivations,
+    interests,
+    onboarding_completed,
+    updated_at
+) on table public.profiles to authenticated;
